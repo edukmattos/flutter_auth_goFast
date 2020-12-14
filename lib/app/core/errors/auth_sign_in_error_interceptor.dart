799bcfea17d4a37a75490228a1aa6d23290121fa
Eduardo Mattos <edukmattos@gmail.com>
2020-12-14 07:25:17 -0300

    Auth - Errors

--- lib/app/core/errors/auth_sign_in_error_interceptor.dart
+++ lib/app/core/errors/auth_sign_in_error_interceptor.dart
@@ -7,33 +7,33 @@ class AuthSignInErrorInterceptor implements IErrorInterceptor {
     switch (error) {
       case 'user-not-found':
         //authStatus = AuthStatus.successful;
-        return 'Acho que essa senha está muito curta, tente uma maior!';
+        return 'Acho que essa senha está muito curta, tente uma maior ! ';
         break;
       case 'wrong-password':
-        return 'Ops! Esse email é inválido!';
+        return 'Ops! Esse email é inválido ! ';
         break;
       case 'user-disabled':
         return 'Conta desabilitada, '
-            'se já tem uma conta basta fazer o login!';
+            'Verifique com o Administrador ! ';
         break;
       case 'operation-not-allowed':
-        return 'Esse email já está cadastrado,'
+        return 'Esse email já está cadastrado, '
             'se já tem uma conta basta fazer o login!';
         break;
       case 'invalid-credencial':
-        return 'Esse email já está cadastrado,'
+        return 'Esse email já está cadastrado, '
             'se já tem uma conta basta fazer o login!';
         break;
       case 'invalid-verification-code':
-        return 'Esse email já está cadastrado,'
+        return 'Esse email já está cadastrado, '
             'se já tem uma conta basta fazer o login!';
         break;
       case 'invalid-verification-id':
-        return 'Esse email já está cadastrado,'
+        return 'Esse email já está cadastrado, '
             'se já tem uma conta basta fazer o login!';
         break;
       case 'account-exists-with-different-credencial':
-        return 'Esse email já está cadastrado,'
+        return 'Esse email já está cadastrado, '
             'se já tem uma conta basta fazer o login!';
         break;
 
